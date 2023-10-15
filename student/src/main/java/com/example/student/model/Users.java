11650314143e5651a61375360b1ca25f46b8ce67
Collins Daberechukwu <collinsdaberechi20@gmail.com>
2023-10-15 04:13:06 +0100

    yml configuration and Student service update

---
+++ student/src/main/java/com/example/student/model/Users.java
@@ -0,0 +1,26 @@
+package com.example.student.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name="users")
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int Age;
+    private String jambScore;
+    private String address;
+    private String phoneNumber;
+}
