f4d90d1ff73fa3ae1ca875b5e5bf74a757098b3d
Collins Daberechukwu <collinsdaberechi20@gmail.com>
2023-10-15 23:32:29 +0100

    Student Repo

---
+++ student/src/main/java/com/example/student/repository/UserRepository.java
@@ -0,0 +1,7 @@
+//package com.example.student.repository;
+//
+//import com.example.student.model.Users;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public UserRepository extends JpaRepository<Users,Long> {
+//}
